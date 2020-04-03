9cf8206a54abc51395d2e3f42405c8f41fde35b4
DmitriySemenov91 <48211585+DmitriySemenov91@users.noreply.github.com>
2020-04-03 12:40:21 +0200

    feat: update index.js
    solution working only 70 %

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsConfigSize = bracketsConfig.length;
+
+    for (let i = 0; i < bracketsConfigSize;) {
+
+        let find = str.includes(bracketsConfig[i].join(''));
+
+        if (find) {
+            str = str.replace(/\S./g, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    return str === '';
 }
